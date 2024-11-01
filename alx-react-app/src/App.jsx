454a2464e84cdd876130029c52bf86815396597e
Immanuel104 <ndundaimmanuel61@gmail.com>
2024-11-01 14:56:44 +0300

    my files

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,9 +1,15 @@
-// Importing the WelcomeMessage component
+import React from 'react';
+import Header from './components/Header';  // Importing Header component
+import MainContent from './components/MainContent';  // Importing MainContent component
+import Footer from './components/Footer';  // Importing Footer component// Importing the WelcomeMessage component
 import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
     return (
         <div>
+           <Header />           {/* Including Header component */}
+            <MainContent />      {/* Including MainContent component */}
+            <Footer />           {/* Including Footer component */}
             <WelcomeMessage />
         </div>
     );
