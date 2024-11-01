7516154237bae103afdb444b42d096bd4d7605c2
Immanuel104 <ndundaimmanuel61@gmail.com>
2024-11-01 15:07:54 +0300

    my files

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import UserProfile from './components/UserProfile';  // Importing UserProfile componentimport React from 'react';
 import Header from './components/Header';  // Importing Header component
 import MainContent from './components/MainContent';  // Importing MainContent component
 import Footer from './components/Footer';  // Importing Footer component// Importing the WelcomeMessage component
@@ -9,6 +9,7 @@ function App() {
         <div>
            <Header />           {/* Including Header component */}
             <MainContent />      {/* Including MainContent component */}
+               <UserProfile name="Immanuel" age="20" bio="Loves hiking and photography." />  {/* Using UserProfile component */}
             <Footer />           {/* Including Footer component */}
             <WelcomeMessage />
         </div>
