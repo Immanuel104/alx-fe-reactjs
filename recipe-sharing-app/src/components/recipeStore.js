2933824829599c7e70a3f57275ac0291eb643590
Immanuel104 <ndundaimmanuel61@gmail.com>
2024-11-15 19:18:38 +0300

    my files

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -5,7 +5,7 @@ const useRecipeStore = create(set => ({
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   deleteRecipe: (recipeId) => set(state => ({
     recipes: state.recipes.filter(recipe => recipe.id !== recipeId)
-  })),
+  })),setSearchTerm", "searchTerm"
   updateRecipe: (updatedRecipe) => set(state => ({
     recipes: state.recipes.map(recipe =>
       recipe.id === updatedRecipe.id ? updatedRecipe : recipe
