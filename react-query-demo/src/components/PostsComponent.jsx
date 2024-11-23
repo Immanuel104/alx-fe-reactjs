0fa1547ae9ae85295034089d37118e50357561b5
Immanuel104 <ndundaimmanuel61@gmail.com>
2024-11-23 14:19:13 +0300

    my files

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+
+const fetchPosts = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+};
+
+function PostsComponent() {
+  const { data, isLoading, isError, refetch } = useQuery('posts', fetchPosts, {
+    staleTime: 5000, // Data is considered fresh for 5 seconds
+    cacheTime: 10000, // Cached data is retained for 10 seconds
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error fetching posts.</div>;
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <button onClick={() => refetch()}>Refetch Data</button>
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default PostsComponent;
