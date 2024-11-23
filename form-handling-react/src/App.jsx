f958b107061c9165a379dbd9b84ee6f1fa6e9162
Immanuel104 <ndundaimmanuel61@gmail.com>
2024-11-23 13:30:00 +0300

    my files

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,5 +1,20 @@
 import React, { useState } from "react";
 import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/formikForm";
+
+const App = () => {
+  const [useFormik, setUseFormik] = useState(false);
+
+  return (
+    <div>
+      <h1>User Registration</h1>
+      <button onClick={() => setUseFormik(!useFormik)}>
+        Switch to {useFormik ? "Controlled Components" : "Formik"}
+      </button>
+      {useFormik ? <FormikForm /> : <RegistrationForm />}
+    </div>
+  );import React, { useState } from "react";
+import RegistrationForm from "./components/RegistrationForm";
 import FormikForm from "./components/FormikForm";
 
 const App = () => {
