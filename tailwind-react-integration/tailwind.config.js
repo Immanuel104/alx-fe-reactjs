9100832a848e2e91a62b2ba718c76c61c480cf7f
Immanuel104 <ndundaimmanuel61@gmail.com>
2024-11-28 16:29:29 +0300

    my files

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}","purge", "variants" "./public/index.html"],
   darkMode: false, // You can enable dark mode if needed
   theme: {
     extend: {},
