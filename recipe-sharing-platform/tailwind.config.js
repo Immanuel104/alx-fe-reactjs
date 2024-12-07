5aafad1b2957aa4109b8729a0219fc3af4788d63
Immanuel104 <ndundaimmanuel61@gmail.com>
2024-12-07 11:27:34 +0300

    my files

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // Can be set to 'media' or 'class' if needed for dark mode
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
